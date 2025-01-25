b40ab0575fca97fb8882744a9ae92161a9cf5a9c
PrabhasKumarSreerama <sreeramaprabhaskumar18@gmail.com>
2025-01-25 14:24:21 +0530

    removed extra folder

--- server copy/routes/vehicleRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const { getVehicles, addVehicle } = require('../controllers/vehicleController');
-const { protect, admin } = require('../middleware/authMiddleware');
-const router = express.Router();
-
-router.route('/').get(getVehicles).post(protect, admin, addVehicle);
-
-module.exports = router;
